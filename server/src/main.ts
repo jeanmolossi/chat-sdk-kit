85b397237deffffe38cb30457a660e119cead385
jeanmolossi <jean.carlosmolossi@gmail.com>
2021-01-29 00:28:50 -0300

    chore: added app module and run server application

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,8 @@
-import { NestFactory } from '@nestjs/core';
 import { ServerApplication } from '@/application/ServerApplication';
 
 async function bootstrap() {
-  const app = await NestFactory.create({});
-  await app.listen(3000);
+  const serverApplication: ServerApplication = ServerApplication.new();
+  serverApplication.run();
 }
+
 bootstrap();
