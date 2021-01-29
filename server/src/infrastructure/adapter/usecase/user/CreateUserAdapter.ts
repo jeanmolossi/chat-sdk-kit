04d4d2413579e6ca74879332f6e28deffe45f990
jeanmolossi <jean.carlosmolossi@gmail.com>
2021-01-29 04:16:57 -0300

    chore: addedd graphql compatibility and documentation on adapter

--- server/src/infrastructure/adapter/usecase/user/CreateUserAdapter.ts
+++ server/src/infrastructure/adapter/usecase/user/CreateUserAdapter.ts
@@ -1,6 +1,7 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { Exclude, Expose, plainToClass } from 'class-transformer';
 import { IsOptional, IsString, IsUrl, IsUUID } from 'class-validator';
+import { v4 } from 'uuid';
 import { UseCaseValidatableAdapter } from '@/core/common';
 import { ICreateUserUsePort } from '@/core/domain/user';
 
@@ -12,28 +13,43 @@ export class CreateUserAdapter
   @Expose()
   @IsUUID(4, { message: 'O Identificador deve ser um uuid v4 válido' })
   @IsOptional()
-  @Field(() => String, { nullable: true })
-  id?: string;
+  @Field(() => String, {
+    nullable: true,
+    description: 'Campo identificador gerado automaticamente',
+  })
+  id?: string = v4();
 
   @Expose()
   @IsString({ message: 'O nome deve ser válido' })
-  @Field(() => String, { nullable: false })
+  @Field(() => String, {
+    nullable: false,
+    description: 'Campo com o nome do usuário',
+  })
   name: string;
 
   @Expose()
   @IsUrl({}, { message: 'A Foto deve ser uma url válida' })
   @IsOptional()
-  @Field(() => String, { nullable: true })
+  @Field(() => String, {
+    nullable: true,
+    description: 'Campo com a Url da photo do perfil do usuário',
+  })
   photo?: string;
 
   @Expose()
   @IsOptional()
-  @Field(() => Date, { nullable: true })
+  @Field(() => Date, {
+    nullable: true,
+    description: 'Campo Date gerado automaticamente',
+  })
   created_at?: Date;
 
   @Expose()
   @IsOptional()
-  @Field(() => Date, { nullable: true })
+  @Field(() => Date, {
+    nullable: true,
+    description: 'Campo Date gerado automaticamente',
+  })
   updated_at?: Date;
 
   static async new(payload: ICreateUserUsePort): Promise<CreateUserAdapter> {
