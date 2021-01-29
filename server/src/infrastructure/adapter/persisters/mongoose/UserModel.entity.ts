540086866f7845a40a032749be3e4bbb2c62b04e
jeanmolossi <jean.carlosmolossi@gmail.com>
2021-01-29 04:17:36 -0300

    fix: id typo to mongo recognize

--- server/src/infrastructure/adapter/persisters/mongoose/UserModel.entity.ts
+++ server/src/infrastructure/adapter/persisters/mongoose/UserModel.entity.ts
@@ -1,12 +1,13 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { v4 } from 'uuid';
 
 export type UserDocument = UserModel & Document;
 
 @Schema()
 export class UserModel {
-  @Prop()
-  id: string;
+  @Prop({ type: String, default: v4() })
+  _id: string;
 
   @Prop({ required: true })
   name: string;
