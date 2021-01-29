35e0233be0d90e279ac8a4f80c35ac3cbc98d6bb
jeanmolossi <jean.carlosmolossi@gmail.com>
2021-01-29 04:18:00 -0300

    chore: created CreateUserInput model

--- server/src/application/graphql/inputs/user/CreateUserInput.ts
+++ server/src/application/graphql/inputs/user/CreateUserInput.ts
@@ -3,18 +3,33 @@ import { ICreateUserUsePort } from '@/core/domain/user';
 
 @InputType()
 export class CreateUserInput implements ICreateUserUsePort {
-  @Field(() => String, { nullable: true })
+  @Field(() => String, {
+    nullable: true,
+    description: 'Não precisa enviar, será gerado automáticamente',
+  })
   id?: string;
 
-  @Field(() => String, { nullable: false })
+  @Field(() => String, {
+    nullable: false,
+    description: 'Campo com o nome do usuário',
+  })
   name: string;
 
-  @Field(() => String, { nullable: true })
+  @Field(() => String, {
+    nullable: true,
+    description: 'Url da foto de perfil do usuário',
+  })
   photo?: string;
 
-  @Field(() => Date, { nullable: true })
+  @Field(() => Date, {
+    nullable: true,
+    description: 'Campo Date gerado automaticamente',
+  })
   created_at?: Date;
 
-  @Field(() => Date, { nullable: true })
+  @Field(() => Date, {
+    nullable: true,
+    description: 'Campo Date gerado automaticamente',
+  })
   updated_at?: Date;
 }
