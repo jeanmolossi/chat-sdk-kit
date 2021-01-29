0926e29e351a672f7a4e170b2fa454730570a91c
jeanmolossi <jean.carlosmolossi@gmail.com>
2021-01-29 02:20:38 -0300

    chore: added find user by id usecase token

--- server/src/core/domain/user/di/UserDITokens.ts
+++ server/src/core/domain/user/di/UserDITokens.ts
@@ -2,4 +2,8 @@ export class UserDITokens {
   static readonly CreateUserUseCase: unique symbol = Symbol(
     'CreateUserUseCase',
   );
+
+  static readonly FindUserByIdUseCase: unique symbol = Symbol(
+    'FindUserByIdUseCase',
+  );
 }
