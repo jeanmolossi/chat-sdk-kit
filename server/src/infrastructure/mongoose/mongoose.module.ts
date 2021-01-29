66de2d6d4bb926dd6d1d96bb1dc1feff89d7b25d
jeanmolossi <jean.carlosmolossi@gmail.com>
2021-01-29 04:16:12 -0300

    refactor: mongoose module

--- server/src/infrastructure/mongoose/mongoose.module.ts
+++ server/src/infrastructure/mongoose/mongoose.module.ts
@@ -1,16 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule as NestMongooseModule } from '@nestjs/mongoose';
-import {
-  UserModel,
-  UserSchema,
-} from '../adapter/persisters/mongoose/UserModel.entity';
 
 @Module({
-  imports: [
-    NestMongooseModule.forRoot('mongodb://localhost/chat-sdk'),
-    NestMongooseModule.forFeature([
-      { name: UserModel.name, schema: UserSchema },
-    ]),
-  ],
+  imports: [NestMongooseModule.forRoot('mongodb://localhost/chat-sdk')],
+  exports: [NestMongooseModule],
 })
 export class MongooseModule {}
