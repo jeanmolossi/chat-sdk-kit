41ab283897022b992d0a5114b883fc9c40c6e391
jeanmolossi <jean.carlosmolossi@gmail.com>
2021-01-29 01:13:17 -0300

    refactor: query hello world documented

--- server/src/application/graphql/resolvers/hello-world.ts
+++ server/src/application/graphql/resolvers/hello-world.ts
@@ -2,7 +2,11 @@ import { Resolver, Query } from '@nestjs/graphql';
 
 @Resolver()
 export default class HelloWorld {
-  @Query(() => String!)
+  @Query(() => String!, {
+    name: 'helloWorld',
+    description: 'Mensagem de Hello World',
+    nullable: false,
+  })
   helloWorld() {
     return `Hello World`;
   }
