ae77c9fcb9a6ca847fcb3ad1547be581d448b103
jeanmolossi <jean.carlosmolossi@gmail.com>
2021-01-29 04:35:37 -0300

    chore: exported find usecases

--- server/src/core/domain/user/index.ts
+++ server/src/core/domain/user/index.ts
@@ -6,7 +6,9 @@ export * from './entity/common-types';
 export * from './port/peristence/UserRepositoryPort';
 
 export * from './port/usecase/CreateUserUseCasePort';
+export * from './port/usecase/FindUserByIdUseCasePort';
 
 export * from './usecase/dto/UserUseCaseDTO';
 
 export * from './usecase/CreateUserUseCase';
+export * from './usecase/FindUserByIdUseCase';
