2e44c6ce4c0dd4c6ad97aefc78ee678ec036aa16
jeanmolossi <jean.carlosmolossi@gmail.com>
2021-01-29 04:16:33 -0300

    chore: added documentation on CoreApiResponseAdapter

--- server/src/infrastructure/graphql/graphql-response-adapter.ts
+++ server/src/infrastructure/graphql/graphql-response-adapter.ts
@@ -2,12 +2,21 @@ import { Field, Float, Int, ObjectType } from '@nestjs/graphql';
 
 @ObjectType()
 export class CoreApiResponseType {
-  @Field(() => Int, { nullable: false })
+  @Field(() => Int, {
+    nullable: false,
+    description: 'Campo com o código de status da resposta da requisição',
+  })
   statusCode: number;
 
-  @Field(() => String, { nullable: false })
+  @Field(() => String, {
+    nullable: false,
+    description: 'Mensagem de feedback da requisição',
+  })
   message: string;
 
-  @Field(() => Float, { nullable: false })
+  @Field(() => Float, {
+    nullable: false,
+    description: 'Unix timestamp do momento da requisição',
+  })
   timestamp: number;
 }
