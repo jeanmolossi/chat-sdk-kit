2f37b2e77ca966bbc120fa82522830b77dc141f7
jeanmolossi <jean.carlosmolossi@gmail.com>
2021-01-29 04:18:24 -0300

    chore: resolved injections

--- server/src/application/modules/user.module.ts
+++ server/src/application/modules/user.module.ts
@@ -1,28 +1,42 @@
 import { Module, Provider } from '@nestjs/common';
-import { UserModel } from '@/infrastructure/adapter/persisters/mongoose/UserModel.entity';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  UserModel,
+  UserSchema,
+} from '@/infrastructure/adapter/persisters/mongoose/UserModel.entity';
+import { UserModelRepository } from '@/infrastructure/adapter/persisters/mongoose/UserModel.repository';
 import { UserDITokens } from '@/core/domain/user';
 import { CreateUserService } from '@/core/service/user/CreateUserService';
 import { CreateUserMutation } from '../graphql/mutations/create-user-mutation';
 
-const PersisterProviders: Provider[] = [];
+const PersisterProviders: Provider[] = [
+  {
+    provide: UserDITokens.UserRepositoryToken,
+    useClass: UserModelRepository,
+  },
+];
 
 const UseCaseProviders: Provider[] = [
-  UserModel,
   {
     provide: UserDITokens.CreateUserUseCase,
-    inject: [UserModel.name],
+    inject: [UserDITokens.UserRepositoryToken],
     useFactory: userRepository => new CreateUserService(userRepository),
   },
 ];
 
 const ResolversProviders: Provider[] = [CreateUserMutation];
 
+const MongooseModuleImport = MongooseModule.forFeature([
+  { name: UserModel.name, schema: UserSchema },
+]);
+
 @Module({
+  imports: [MongooseModuleImport],
   providers: [
     ...PersisterProviders,
     ...UseCaseProviders,
     ...ResolversProviders,
   ],
-  exports: [UserModel.name],
+  exports: [UserDITokens.UserRepositoryToken],
 })
 export class UserModule {}
