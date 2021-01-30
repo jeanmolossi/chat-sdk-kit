e0ad7f48eaab40541414e91a58082b06d6c0493e
jeanmolossi <jean.carlosmolossi@gmail.com>
2021-01-30 12:50:01 -0300

    refactor: added findUser method to userRepository

--- server/src/core/domain/user/port/peristence/UserRepositoryPort.ts
+++ server/src/core/domain/user/port/peristence/UserRepositoryPort.ts
@@ -1,7 +1,8 @@
-import { Optional } from '@/core/common';
-import { User } from '../../entity/User';
+import { IQueryBy, Optional, RepositoryOptions } from '@/core/common';
+import { User } from '@/core/domain/user';
 
 export interface IUserRepositoryPort {
   createUser: (user: User) => Promise<User>;
+  findUser(by: IQueryBy, options?: RepositoryOptions.FindOptions);
   findUserById: (userId: string) => Promise<Optional<User>>;
 }
