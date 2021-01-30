5abe195cd9d98ba5edf75e912cb74fe7865253e7
jeanmolossi <jean.carlosmolossi@gmail.com>
2021-01-30 15:56:43 -0300

    chore: refactored user model

--- server/src/infrastructure/adapter/persisters/mongoose/user/UserModel.entity.ts
+++ server/src/infrastructure/adapter/persisters/mongoose/user/UserModel.entity.ts
@@ -1,23 +1,25 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 import { v4 } from 'uuid';
+import { Optional } from '@/core/common';
 import { UserRole } from '@/core/common/enums/UserRole';
+import { UserType } from '@/core/domain/user';
 
 export type UserDocument = UserModel & Document;
 
 @Schema()
-export class UserModel {
+export class UserModel implements UserType.IModel {
   @Prop({ type: String, default: v4() })
   _id: string;
 
   @Prop({ required: true })
   name: string;
 
-  @Prop()
-  photo?: string;
+  @Prop({ type: String })
+  photo: Optional<string>;
 
-  @Prop()
-  role?: UserRole;
+  @Prop({ type: String })
+  role: Optional<UserRole>;
 
   @Prop()
   created_at?: Date;
