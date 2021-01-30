fea75d7009b44154c5a719e7d38a6ccae798962f
jeanmolossi <jean.carlosmolossi@gmail.com>
2021-01-30 13:08:17 -0300

    chore: added bus adapters

--- server/src/application/modules/infrastructure.module.ts
+++ server/src/application/modules/infrastructure.module.ts
@@ -1,13 +1,39 @@
-import { Global, Module } from '@nestjs/common';
+import { Global, Module, Provider } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
+import { NestCommandBusAdapter } from '@/infrastructure/adapter/message/NestCommandBusAdapter';
+import { NestEventBusAdapter } from '@/infrastructure/adapter/message/NestEventBusAdapter';
+import { NestQueryBusAdapter } from '@/infrastructure/adapter/message/NestQueryBusAdapter';
 import { EnvironmentModule } from '@/infrastructure/config/environment/environment.module';
 import { GraphqlModule } from '@/infrastructure/config/graphql/graphql.module';
 import { MongooseModule } from '@/infrastructure/config/mongoose/mongoose.module';
+import { CoreDITokens } from '@/core/common';
 import HelloWorld from '../graphql/resolvers/hello-world';
-import { UserModule } from './user.module';
+
+const providers: Provider[] = [
+  {
+    provide: CoreDITokens.CommandBus,
+    useClass: NestCommandBusAdapter,
+  },
+  {
+    provide: CoreDITokens.QueryBus,
+    useClass: NestQueryBusAdapter,
+  },
+  {
+    provide: CoreDITokens.EventBus,
+    useClass: NestEventBusAdapter,
+  },
+];
+
+const resolverProviders: Provider[] = [HelloWorld];
 
 @Global()
 @Module({
-  imports: [EnvironmentModule, GraphqlModule, MongooseModule, UserModule],
-  providers: [HelloWorld],
+  imports: [CqrsModule, EnvironmentModule, GraphqlModule, MongooseModule],
+  providers: [...providers, ...resolverProviders],
+  exports: [
+    CoreDITokens.CommandBus,
+    CoreDITokens.QueryBus,
+    CoreDITokens.EventBus,
+  ],
 })
 export class InfrastructureModule {}
