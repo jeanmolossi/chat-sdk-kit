d5d04a4253d3e20631b3e11d953444b9a4eed3ee
jeanmolossi <jean.carlosmolossi@gmail.com>
2021-01-31 17:52:28 -0300

    chore: added schema decorator

--- server/src/infrastructure/adapter/persisters/mongoose/message/MessageModel.entity.ts
+++ server/src/infrastructure/adapter/persisters/mongoose/message/MessageModel.entity.ts
@@ -1,10 +1,11 @@
-import { Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 import { v4 } from 'uuid';
 import { MessageType } from '@/core/domain/message';
 
 export type MessageDocument = MessageModel & Document;
 
+@Schema()
 export class MessageModel implements MessageType.IMessageModel {
   @Prop({ type: String, default: v4() })
   _id: string;
