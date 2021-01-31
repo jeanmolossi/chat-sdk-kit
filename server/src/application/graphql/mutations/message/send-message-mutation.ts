06af699824a2faba726400dea308a4fec7a9c62b
jeanmolossi <jean.carlosmolossi@gmail.com>
2021-01-31 17:45:19 -0300

    chore: created send message mutation

---
+++ server/src/application/graphql/mutations/message/send-message-mutation.ts
@@ -0,0 +1,35 @@
+import { Inject } from '@nestjs/common';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { MessageResponseData } from '@/infrastructure/adapter/graphql/message/MessageDTOAdapter';
+import { SendMessageAdapter } from '@/infrastructure/adapter/usecase/message/SendMessageAdapter';
+import { CoreApiResponse } from '@/core/common';
+import {
+  MessageDITokens,
+  MessageUseCaseDTO,
+  SendMessageUseCase,
+} from '@/core/domain/message';
+import { SendMessageInput } from '../../inputs/message/SendMessageInput';
+
+@Resolver()
+export class SendMessageMutation {
+  constructor(
+    @Inject(MessageDITokens.SendMessageUseCase)
+    private readonly sendMessageUseCase: SendMessageUseCase,
+  ) {}
+
+  @Mutation(() => MessageResponseData)
+  async sendMessage(
+    @Args({
+      type: () => SendMessageInput,
+      description: 'Corpo da mensagem',
+      name: 'message',
+    })
+    message: SendMessageInput,
+  ): Promise<CoreApiResponse<MessageUseCaseDTO>> {
+    const adapter: SendMessageAdapter = await SendMessageAdapter.new(message);
+
+    const sentMessage = await this.sendMessageUseCase.execute(adapter);
+
+    return CoreApiResponse.success(sentMessage);
+  }
+}
