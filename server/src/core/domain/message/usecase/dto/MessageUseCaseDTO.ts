56d6f7cf6c859010846d3b64bb2a13341641b85f
jeanmolossi <jean.carlosmolossi@gmail.com>
2021-01-31 16:36:00 -0300

    chore: created mapper newFromMessage dto

--- server/src/core/domain/message/usecase/dto/MessageUseCaseDTO.ts
+++ server/src/core/domain/message/usecase/dto/MessageUseCaseDTO.ts
@@ -1,12 +1,29 @@
+import { Exclude, Expose, plainToClass } from 'class-transformer';
 import { Optional } from '@/core/common';
 import { MessageType } from '../../entity/common-types';
+import { Message } from '../../entity/Message';
 
+@Exclude()
 export class MessageUseCaseDTO implements MessageType.IMessageModel {
+  @Expose()
   text: string;
 
+  @Expose()
   user: string;
 
+  @Expose()
   created_at?: Optional<Date>;
 
+  @Expose()
   updated_at?: Optional<Date>;
+
+  static newFromMessage(message: Message): MessageUseCaseDTO {
+    const dto: MessageUseCaseDTO = plainToClass(MessageUseCaseDTO, message);
+
+    return dto;
+  }
+
+  static newFromMessageList(messages: Message[]): MessageUseCaseDTO[] {
+    return messages.map(message => this.newFromMessage(message));
+  }
 }
